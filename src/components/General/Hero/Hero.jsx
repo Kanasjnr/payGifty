7fd466b9268503a8c78db8f3cda929d3fc57488a
Bayz.ng <ayofe70@gmail.com>
2024-07-10 16:13:56 -0700

    adjust

--- src/components/General/Hero/Hero.jsx
+++ src/components/General/Hero/Hero.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-import '../Hero/Hero.css'
-import { heroImg } from '../../../assets'
+import React from "react";
+import "../Hero/Hero.css";
+import { heroImg } from "../../../assets";
 
 const Hero = () => {
   return (
-    <div className='heroCon'>
+    <div className="heroCon">
       <div>
-        <h5 className='heroBig'>With <span>PayGifty</span> Surprise your <br/> loved ones with affordable gifts  <br/>In An Easy Way</h5>
+        <h5 className="heroBig">
+          With <span>PayGifty</span> Surprise your <br /> loved ones with
+          affordable gifts <br />
+          In An Easy Way
+        </h5>
 
-          <p>Lorem ipsum dolor, sit amet consectetur <br/>
-            adipisicing elit. Libero facere ipsum nesciunt odio.</p>
+        <p>
+          Make your loved one's day extra special by surprising them with a thoughtful and affordable gift. Whether it's for a birthday, anniversary, or just to show them how much you care, finding the perfect present doesn't have to break the bank.
+        </p>
 
-            <button>Get Started</button>
+        <button>Get Started</button>
       </div>
       <div>
-        <img src={heroImg} alt="heropic"  className='heroPic'/>
+        <img src={heroImg} alt="heropic" className="heroPic" />
       </div>
     </div>
-    
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
