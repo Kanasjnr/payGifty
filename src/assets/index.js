92edb60f387a208b098bee19425bd7deed9edf16
Mentor-Ntech <102734739+Mentor-Ntech@users.noreply.github.com>
2024-07-10 15:25:12 +0100

    home update

---
+++ src/assets/index.js
@@ -0,0 +1,11 @@
+import Hero from './heroImg.png';
+import gift from "./giftCard.png";
+import redeem from "./redeeemGift.png";
+import design from "./designGift.png"
+
+export {
+    Hero as heroImg,
+    gift as giftImg,
+    redeem as redeemImg,
+    design as designImg
+}
