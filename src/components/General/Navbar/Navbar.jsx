7c6c6f405dfab93d21d9e291c48f6f692f9719e2
Bayz.ng <ayofe70@gmail.com>
2024-07-10 15:35:49 -0700

    bayo commit

--- src/components/General/Navbar/Navbar.jsx
+++ src/components/General/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import "./Navbar.css"
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='Navbar'>
+      <div>
+        <h2>payGifty</h2>
+      </div>
+      <div className='--Navbar-content'>
+        <h3>Home</h3>
+        <h3>Contact Us</h3>
+        <h3>About</h3>
+      </div>
+      <div className='--Navbar-button'>
+        <button>Sign Up</button>
+        <button>Log In</button>
+      </div>
+    </div>
   )
 }
 
